ba447058e0ec81adf54acd907c32a63c5df0676c
jota12x <juanjose.tenorio@pucp.pe>
2018-01-04 12:07:42 -0500

    UVA_10003

---
+++ Dynamic_Programming/UVA/UVA_10003_Cutting_Sticks.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+
+#define f(i,a,b) for(int i = a ; i < b ; i++)
+using namespace std;
+
+int dp[52][52]={{0}},cuts[52];
+
+int solve(int start, int end){
+  int minCost = 100000;
+  if(start + 1 == end){
+    return (dp[start][end] = 0);
+  }
+  if(dp[start][end]!=0){ //memoization step : it's not necesary to compute values that have been computed before
+    return dp[start][end];
+  }
+  //Iterate through all posible cuts
+  for(int i = start +1; i<end;i++){
+    //cout<<"Solving for " << start << "-"<<end<<endl;
+    minCost = min(minCost,solve(start,i) + solve(i,end) + (cuts[end] - cuts[start]));
+  }
+  dp[start][end] = minCost;
+  return dp[start][end];
+}
+int main(){
+  int l,n, minCost;
+  /*Top down DYNAMIC PROGRAMMING*/
+  while(cin >>l && l){
+
+    //inicialization
+    memset(dp,0, sizeof(dp));
+    memset(cuts,0,sizeof(cuts));
+
+    //Input reading
+    cin>>n;
+    cuts[0]=0;
+    for(int i=1;i<n+1;i++){
+      cin>>cuts[i];
+    }
+    cuts[n+1] = l;
+
+    //Solving phase
+    minCost = solve(0,n+1);
+    cout<<"The minimum cutting is " <<minCost<<"."<<endl;
+  }
+
+}
---
+++ Dynamic_Programming/template.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <iomanip>
+#include <cctype>
+#include <stack>
+#include <climits>
+#include <bitset>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <iterator>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#define FOR(i,A) for(typeof (A).begin() i = (A).begin() ; i != (A).end() ; i++)
+#define f(i,a,b) for(int i = a ; i < b ; i++)
+#define fd(i,a,b) for(int i = a ; i >= b ; i--)
+#define debug( x ) cout << #x << " = " << x << endl
+#define clr(v,x) memset( v, x , sizeof v )
+#define all(x) (x).begin() , (x).end()
+#define mp make_pair
+#define pb push_back
+#define SZ(a) int(a.size())
+#define sc second
+#define fr first
+#define rall(x) (x).rbegin() , (x).rend()
+#define PI acos( -1.0 )
+#define EPS 1E-9
+#define MAXM 100010
+#define MAXN 100010
+using namespace std;
+
+typedef pair<int,int> ii ;
+typedef long long ll ;
+typedef long double ld ;
+typedef pair<int,ii> pii ;
+typedef vector <ii> vii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+int M,N;
+bool obstacle[50][50];
+bool visited[51][51][4];
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
